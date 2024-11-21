8d3056f77ee5959b83c79ab9e75513b41305efbe
Anjitkumaryadav <anjitaniket@gmail.com>
2024-11-21 18:24:31 +0530

    chat app

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express();
+const http = require("http");
+const cors = require("cors")
+const {Server} = require("socket.io");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ['GET', 'POST']
+    },
+});
+
+io.on("connection",(socket)=>{
+    console.log(`User connected : ${socket.id}`)
+
+    socket.on("send-message", (message)=>{
+        
+        //Broadcast the recieved message
+        io.emit("recieved-message",message)
+        console.log(message)
+    })
+    socket.on("disconnected", ()=> console.log("user is disconnected"))
+})
+
+server.listen(5000,()=>console.log("Server is running at port 5000"))
